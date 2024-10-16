e249b766b2d58544387b13275b9df96c83253c2b
shivanand_khudanapur <155430597+shiv-sk@users.noreply.github.com>
2024-10-16 09:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (99.97%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        int k = 0;
         for(int i = 0; i<nums.size(); i++){
-            if(nums[i] == val){
-                nums.erase(nums.begin() + i);
-                i--;
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return nums.size();
+        return k;
     }
 };
