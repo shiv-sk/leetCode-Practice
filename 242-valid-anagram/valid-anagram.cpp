2defbdfa1b560e55af1c56afea8d675128e1e48c
shivanand_khudanapur <155430597+shiv-sk@users.noreply.github.com>
2024-12-14 10:39:38 +0530

    Time: 4 ms (52.67%) | Memory: 9.6 MB (53.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(t.size() != s.size()){
-            return false;
-        }
-        unordered_map<char , int>umap;
-        for(char ch : s){
-            umap[ch]++;
-        }
-        for(char c : t){
-            if(umap.find(c) == umap.end() || umap[c] <= 0){
-                return false;
-            }
-            umap[c]--;
-        }
-        return true;
+        sort(s.begin() , s.end());
+        sort(t.begin() , t.end());
+        return s == t;
     }
 };
